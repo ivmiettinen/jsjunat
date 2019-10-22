efb9804f35c955dc967f8536d84a1915b98d4b51
Rasmus Rajasalo <rarasmus@outlook.com>
2019-10-22 14:27:24 +0300

    Get "selected" train info

--- rasmus/traininfo.js
+++ rasmus/traininfo.js
@@ -1,9 +1,19 @@
 var selectedTrain = 1;
+var info = document.getElementById('trainInfo');
+var optiot = { hour: '2-digit', minute: '2-digit', hour12: false };
 
-$.get("https://rata.digitraffic.fi/api/v1/trains/2017-01-01/"+selectedTrain+"/", function(data) {
+$.get("https://rata.digitraffic.fi/api/v1/trains/2017-01-01/" + selectedTrain + "/", function (data) {
     $(".result").html(data);
     console.log("Load successful");
     console.dir(data);
+    var jsonData = data;
+    var trainNo = jsonData[0].trainNumber;
+    var lastSta = jsonData[0].timeTableRows.length - 1;
+    var dest = jsonData[0].timeTableRows[lastSta].stationShortCode;
+    var depTime = new Date(jsonData[0].timeTableRows[0].scheduledTime).toLocaleTimeString('fi', optiot);
+    var arrTime = new Date(jsonData[0].timeTableRows[lastSta].scheduledTime).toLocaleTimeString('fi', optiot);
+
+    info.innerText = `Train number: ${trainNo}, Departure Time: ${depTime}, Destination: ${dest}, Arrival Time: ${arrTime}`;
 });
 
 module.exports = selectedTrainData;
