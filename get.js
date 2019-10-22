afa19c1d549bb5ba215bbcbd6216090830b378d6
sormunen <sormunen.roni@gmail.com>
2019-10-22 12:04:41 +0300

    get.js bug fix, roni_test.html created, get.js default stations changed from undefined to hki and tpe respectively

--- get.js
+++ get.js
@@ -1,9 +1,10 @@
-var startStation;
-var endStation;
+var startStation = "HKI";
+var endStation = "TPE";
 
 $.get("http://rata.digitraffic.fi/api/v1/live-trains/station/"+startStation+"/"+endStation+"/", function(data) {
     $(".result").html(data);
     console.log("Load successful");
+    console.dir(data);
 });
 
 module.exports = junaData;
