ffb342a05248f8ba56731a18ba1db0cadaac2f5a
Rasmus Rajasalo <rarasmus@outlook.com>
2019-10-23 16:18:39 +0300

    List next trains as table done

--- scripts/shortTwoSta.js
+++ scripts/shortTwoSta.js
@@ -27,21 +27,52 @@ $('#buttonForSearch').on('click', function () {
         }),
         success: function () {
             junaData(depSta, destSta, function (nextTrainsArr) {
-                for (i = 0; i < 20; i++) {
-                    var liElement = document.createElement('li');
+                for (i = 0; i < nextTrainsArr.length; i++) {
+                    var trElement = document.createElement('tr');
 
-                    var trainNumber = nextTrainsArr[i].trainNumber;
-                    var depDate = nextTrainsArr[i].departureDate;
-                    var lastSta = nextTrainsArr[i].timeTableRows.length - 1;
-                    var dest = nextTrainsArr[i].timeTableRows[lastSta].stationShortCode;
-                    var depTime = new Date(nextTrainsArr[i].timeTableRows[0].scheduledTime).toLocaleTimeString('fi', optiot); //muuta myöhemmin
-                    var arrTime = new Date(nextTrainsArr[i].timeTableRows[lastSta].scheduledTime).toLocaleTimeString('fi', optiot);
+                    for (j = 0; j < nextTrainsArr[i].timeTableRows.length; j++) {
+                        if (depSta === nextTrainsArr[i].timeTableRows[j].stationShortCode && nextTrainsArr[i].timeTableRows[j].type === 'DEPARTURE') {
+                            var tdTrainInfo = document.createElement('td');
+                            var tdTrainNo = document.createElement('td');
+                            var tdDepTime = document.createElement('td');
 
-                    var text = document.createTextNode(`Junan numero: ${trainNumber}, Päivä: ${depDate}, Lähtöaika: ${depTime} ------ Pääteasema: ${dest}, Saapumisaika: ${arrTime}`);
-                    liElement.appendChild(text);
-                    document.getElementById('nextTrains').appendChild(liElement);
+                            var trainCateg = nextTrainsArr[i].trainCategory;
+                            var trainNo = nextTrainsArr[i].trainNumber;
+
+                            var depTime = new Date(nextTrainsArr[i].timeTableRows[j].scheduledTime).toLocaleTimeString('fi', optiot);
+
+
+
+                            // var depDate = nextTrainsArr[i].departureDate;
+                            // var lastSta = nextTrainsArr[i].timeTableRows.length - 1;
+                            // var dest = nextTrainsArr[i].timeTableRows[lastSta].stationShortCode;
+                            // var depTime = new Date(nextTrainsArr[i].timeTableRows[0].scheduledTime).toLocaleTimeString('fi', optiot); //muuta myöhemmin
+                            // var arrTime = new Date(nextTrainsArr[i].timeTableRows[lastSta].scheduledTime).toLocaleTimeString('fi', optiot);
+
+                            // var text = document.createTextNode(`Junan numero ${trainNumber}, Lähtöaika: ${depTime}`);
+                            // tdTrainNo.appendChild(text);
+                            tdTrainInfo.innerText = (`${trainCateg}`);
+                            tdTrainNo.innerText = (`${trainNo}`);
+                            tdDepTime.innerText = (`${depTime}`);
+
+                        }
+                        if (destSta === nextTrainsArr[i].timeTableRows[j].stationShortCode && nextTrainsArr[i].timeTableRows[j].type === 'ARRIVAL') {
+                            var tdArrTime = document.createElement('td');
+                            var arrTime = new Date(nextTrainsArr[i].timeTableRows[j].scheduledTime).toLocaleTimeString('fi', optiot);
+
+                            tdArrTime.innerText = (`${arrTime}`);
+
+                        }
+                    }
+                    trElement.appendChild(tdTrainInfo);
+                    trElement.appendChild(tdTrainNo);
+                    trElement.appendChild(tdDepTime);
+                    trElement.appendChild(tdArrTime);
+                    document.getElementById('schedule').appendChild(trElement);
                 }
                 console.log(nextTrainsArr);
+                var result = document.getElementById('result');
+                result.innerText = (`${departure} - ${arrival}`);
             });
         }
     })
