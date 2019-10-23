6bcda898330b0bdafa46ff93b300757f8464adb7
Ilkka Miettinen <ivmiettinen@gmail.com>
2019-10-23 15:03:25 +0300

    geolokaatioJaValiAsemat

--- scripts/nearestStation.js
+++ scripts/nearestStation.js
@@ -1,1 +1,48 @@
 
+navigator.geolocation.getCurrentPosition(function(position) {
+    console.log('Geolocation permissions granted');
+    console.log('Latitude:' + position.coords.latitude);
+    console.log('Longitude:' + position.coords.longitude);
+    getCurrentPosition();
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+//Geolokaatio koordinaatteina:
+
+var x = document.getElementById("geolocation");
+
+
+function getLocation() {
+    
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+
+
--- scripts/twoStations.js
+++ scripts/twoStations.js
@@ -1,37 +1,123 @@
 
+var optiot = {hour: '2-digit', minute:'2-digit', hour12: false};
 
 
-//Henkilön syöttämät asemat:
 
-$('#buttonForSearch').on('click', function()
+$('#buttonForSearch').on('click', function () {
 
-{
     var departure = $('#start').val();
     var arrival = $('#end').val();
 
-    junaData(departure, arrival);
- 
+    junaData(departure, arrival, function (nextTrainsArr) {
+        for (i=0;i<nextTrainsArr.length;i++){
+            var liElement = document.createElement('li');
+
+           
+            var lahtoasema = nextTrainsArr[i].timeTableRows[0].stationShortCode;
+
+            var trainNumber = nextTrainsArr[i].trainNumber;
+            var depDate = nextTrainsArr[i].departureDate;
+            var lastSta = nextTrainsArr[i].timeTableRows.length - 1;
+            var dest = nextTrainsArr[i].timeTableRows[lastSta].stationShortCode;
+            var depTime = new Date(nextTrainsArr[i].timeTableRows[0].scheduledTime).toLocaleTimeString('fi',optiot); //muuta myöhemmin
+            var arrTime = new Date(nextTrainsArr[i].timeTableRows[lastSta].scheduledTime).toLocaleTimeString('fi', optiot);
+            
+            //stations between main stations:
+            var trs=nextTrainsArr[i].timeTableRows;
+            var tempstr = "";
+            
+
+           
+
+            for (var tr of trs){
+                tempstr+=tr.stationShortCode +", ";
+                var howManyStations = trs.length
+
+            }
+            console.log(tempstr);
+            //Muutetaan array
+            //var parserointi = parseInt(tempstr);
+            
+
+            
 
 
 
-     
-    $('<h2></h2>').appendTo('div')
-    .text(departure + " " + arrival);  
+
+
+
+            var text = document.createTextNode(`Commuter: ${trainNumber}, Lähtöasema: ${lahtoasema}, Lähtee: ${depTime}, Väliasemat: ${howManyStations},
+            ------ Pääteasema: ${dest}, Saapuu: ${arrTime}, `);
+            liElement.appendChild(text);
+            document.getElementById('nextTrains').appendChild(liElement);
+        }
+        console.log(nextTrainsArr);
+    });
+    console.log("ButtonForSearch klik");
 
     
+
+
     
+
+    /*
+    //Toimiva väliasemahaku:
+    var trs=nextTrainsArr[i].timeTableRows;
+            var tempstr="";
+
+           
+
+            for (var tr of trs){
+                tempstr+=tr.stationShortCode +", ";
+            }
+
+
+
+
+
+    //Väliasemat:
+    //stationsBetween {departure, arrival, function (nextTrainsArr) {
+
+    //}
+
+    //var stationsBetween;
+
+
+
+
+    //if (nextTrainsArr[i].trainNumber = 169)
     
-               
+    //nextTrainsArr[i].stationShortCode;
+
+   // valiAsemat(nextTrainsArr[i].stationShortCode (nextTrainsArr){
+        //for (i=0; i<nextTrainsArr[i].stationShortCode.length; i++)
+
+
+/*
+
+        { 
+            var li = document.createElement('li');
+
+        }
+    }
+
+*/
+
+
+    //     $('<h2></h2>').appendTo('div')
+    //     .text(departure + " " + arrival);  
+
+
+    //     const $nextTrain = $("<li></li>");
+    // $("#nextTrains").append($nextTrain);
+
+
 
 
 }
 
 );
 
-
-//Tietojen nouto palvelimelta:
-
-var xhr = new XMLHttpRequest();
 
 
 
