018b7c6b9759b706e2f66a972333849393c06e06
Ilkka Miettinen <ivmiettinen@gmail.com>
2019-10-23 14:24:13 +0300

    geolokaatio

--- scripts/nearestStation.js
+++ scripts/nearestStation.js
@@ -1,1 +1,50 @@
 
+navigator.geolocation.getCurrentPosition(function(position) {
+    console.log('Geolocation permissions granted');
+    console.log('Latitude:' + position.coords.latitude);
+    console.log('Longitude:' + position.coords.longitude);
+    getCurrentPosition();
+  });
+
+
+
+
+
+
+
+//Geolokaatio koordinaatteina:
+
+var x = document.getElementById("geolocation");
+
+
+function getLocation() {
+    
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+
+
+
+
+//Kartta:
+/*
+
+function showPosition(position) {
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+  
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=
+    "+latlon+"&zoom=14&size=400x300&sensor=false&key=YOUR_KEY";
+  
+    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+  }
+
+  */
--- scripts/twoStations.js
+++ scripts/twoStations.js
@@ -1,7 +1,7 @@
 
 var optiot = {hour: '2-digit', minute:'2-digit', hour12: false};
 
-//Henkilön syöttämät
+
 
 $('#buttonForSearch').on('click', function () {
 
@@ -12,20 +12,86 @@ $('#buttonForSearch').on('click', function () {
         for (i=0;i<nextTrainsArr.length;i++){
             var liElement = document.createElement('li');
 
+           
+            var lahtoasema = nextTrainsArr[i].timeTableRows[0].stationShortCode;
+
             var trainNumber = nextTrainsArr[i].trainNumber;
             var depDate = nextTrainsArr[i].departureDate;
             var lastSta = nextTrainsArr[i].timeTableRows.length - 1;
             var dest = nextTrainsArr[i].timeTableRows[lastSta].stationShortCode;
             var depTime = new Date(nextTrainsArr[i].timeTableRows[0].scheduledTime).toLocaleTimeString('fi',optiot); //muuta myöhemmin
             var arrTime = new Date(nextTrainsArr[i].timeTableRows[lastSta].scheduledTime).toLocaleTimeString('fi', optiot);
+            
+            //stations between:
+            var trs=nextTrainsArr[i].timeTableRows;
+            var tempstr = "";
 
-            var text = document.createTextNode(`Commuter: ${trainNumber}, Lähtee: ${depTime} ------ Pääteasema: ${dest}, Saapuu: ${arrTime}`);
+           
+
+            for (var tr of trs){
+                tempstr+=tr.stationShortCode +", ";
+            }
+            
+            //Muutetaan array
+            //var parserointi = parseInt(tempstr);
+            
+
+            var text = document.createTextNode(`Commuter: ${trainNumber}, Lähtöasema: ${lahtoasema}, Lähtee: ${depTime}, Väliasemat: ${tempstr.length},
+            ------ Pääteasema: ${dest}, Saapuu: ${arrTime}, `);
             liElement.appendChild(text);
             document.getElementById('nextTrains').appendChild(liElement);
         }
         console.log(nextTrainsArr);
     });
     console.log("ButtonForSearch klik");
+
+    
+
+
+    
+
+    /*
+    //Toimiva väliasemahaku:
+    var trs=nextTrainsArr[i].timeTableRows;
+            var tempstr="";
+
+           
+
+            for (var tr of trs){
+                tempstr+=tr.stationShortCode +", ";
+            }
+
+
+
+
+
+    //Väliasemat:
+    //stationsBetween {departure, arrival, function (nextTrainsArr) {
+
+    //}
+
+    //var stationsBetween;
+
+
+
+
+    //if (nextTrainsArr[i].trainNumber = 169)
+    
+    //nextTrainsArr[i].stationShortCode;
+
+   // valiAsemat(nextTrainsArr[i].stationShortCode (nextTrainsArr){
+        //for (i=0; i<nextTrainsArr[i].stationShortCode.length; i++)
+
+
+/*
+
+        { 
+            var li = document.createElement('li');
+
+        }
+    }
+
+*/
 
 
     //     $('<h2></h2>').appendTo('div')
@@ -42,10 +108,6 @@ $('#buttonForSearch').on('click', function () {
 
 );
 
-
-//Tietojen nouto palvelimelta:
-
-var xhr = new XMLHttpRequest();
 
 
 
