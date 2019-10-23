d96506c6ffbeb2b0e9b5ed75afdb9f518e252bef
Ilkka Miettinen <ivmiettinen@gmail.com>
2019-10-23 14:24:49 +0300

    geolokaatio

--- scripts/nearestStation.js
+++ scripts/nearestStation.js
@@ -33,18 +33,3 @@ function showPosition(position) {
 
 
 
-
-
-//Kartta:
-/*
-
-function showPosition(position) {
-    var latlon = position.coords.latitude + "," + position.coords.longitude;
-  
-    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=
-    "+latlon+"&zoom=14&size=400x300&sensor=false&key=YOUR_KEY";
-  
-    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
-  }
-
-  */
