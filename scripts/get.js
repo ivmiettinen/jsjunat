65d80d8da3719f6949b430d803121671387f88ff
Rasmus Rajasalo <rarasmus@outlook.com>
2019-10-23 10:42:04 +0300

    Work on twoStations.js

--- scripts/get.js
+++ scripts/get.js
@@ -1,12 +1,12 @@
 
- function junaData(startStation, endStation){
+ function junaData(startStation, endStation, callback){
 //startStation = "HKI";
 
 $.get("http://rata.digitraffic.fi/api/v1/live-trains/station/"+startStation+"/"+endStation+"/", function(data) {
     $(".result").html(data);
     console.log("Load successful");
     console.dir(data);
-    return data;
+    callback(data);
 });
 
 }   
